b991e569d805e944aeac241008a05c0a4230c639
a317677438a <277235580@qq.com>
2017-10-13 20:18:47 +0800

    出库统计明细下载

--- jywz/src/main/java/xft/workbench/backstage/storehouseout/action/StorehouseoutAction.java
+++ jywz/src/main/java/xft/workbench/backstage/storehouseout/action/StorehouseoutAction.java
@@ -11,8 +11,6 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +24,7 @@ import xft.workbench.backstage.base.util.GlobalMessage;
 
 import com.kayak.web.base.exception.KPromptException;
 import com.kayak.web.base.service.abs.ComnServiceAbstract;
+import com.kayak.web.base.sql.SqlResult;
 import com.kayak.web.base.system.KResult;
 
 @Controller
@@ -55,16 +54,17 @@ public class StorehouseoutAction extends ABSBaseController{
             
             GlobalMessage.addMapSessionInfo(params);
 			KResult result = comnService.comnQuery(params);
-			JSONArray array=result.getArrayJson();
-
+			
+			SqlResult sResult=result.getSResult();
+			
             response.setCharacterEncoding("UTF-8");
             // 设置文档打开类型
-            response.setContentType("application/octet-stream;charset=GBK");
+            response.setContentType("application/octet-stream;charset=UTF-8");
             // 设置报文头为attachment响应类型
             response.setHeader("Content-disposition",
                     "attachment;" + FileUtil.encodeFileName(request, fileName));
 
-            byte[] bytes = this.createFile(array);  // 读取字节流
+            byte[] bytes = this.createFile(sResult);  // 读取字节流
 
             if (null == bytes || bytes.length == 0) {
                 throw new KPromptException("文件服务器上不存在此文件！");
@@ -96,28 +96,27 @@ public class StorehouseoutAction extends ABSBaseController{
     }
 	
      
-    private byte[] createFile(JSONArray array) throws Exception{
+    private byte[] createFile(SqlResult sResult) throws Exception{
     	StringBuffer fileContent = new StringBuffer("出库单号,出库类型,物资名称,物资类型,出库数量,规格型号,供应商,出库时间,出库仓库,出库状态");
     	fileContent.append("\r\n");
     	
-    	for(int i=0;i<array.length();i++){
-    		JSONObject obj= (JSONObject)array.get(i);
-    		fileContent.append(obj.getString("putout_code"))
-    					.append(EnumUtil.getDescValueByEnumValue(PutoutType.class,obj.getString("putout_type")))
-    					.append(obj.getString("material_name"))
-    					.append(obj.getString("material_type_name"))
-    					.append(obj.getString("putout_number"))
-    					.append(obj.getString("model"))
-    					.append(obj.getString("supplier"))
-    					.append(obj.getString("putout_date"))
-    					.append(obj.getString("putout_storehouse_name"))
-    					.append(EnumUtil.getDescValueByEnumValue(PutoutStatus.class,obj.getString("putoutstatus")))
-    					.append(obj.getString("\r\n"));
+    	while(sResult.next()){
+    		fileContent.append(sResult.getString("putout_code")+",")
+    					.append(EnumUtil.getDescValueByEnumValue(PutoutType.class,sResult.getString("putout_type"))+",")
+    					.append(sResult.getString("material_name")+",")
+    					.append(sResult.getString("material_type_name")+",")
+    					.append(sResult.getString("putout_number")+",")
+    					.append(sResult.getString("model")+",")
+    					.append(sResult.getString("supplier")+",")
+    					.append(sResult.getString("putout_date")+",")
+    					.append(sResult.getString("putout_storehouse_name")+",")
+    					.append(EnumUtil.getDescValueByEnumValue(PutoutStatus.class,sResult.getString("putoutstatus")))
+    					.append("\r\n");
     	}
     	
     	
     	
-    	return fileContent.toString().getBytes();
+    	return fileContent.toString().getBytes("UTF-8");
     	
     }
 	
