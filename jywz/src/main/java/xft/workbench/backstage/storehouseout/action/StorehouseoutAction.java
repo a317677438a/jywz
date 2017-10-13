661a9332cdf30d74a07a9df334f916c085a1a89f
a317677438a <277235580@qq.com>
2017-10-13 20:45:01 +0800

    入库统计

--- jywz/src/main/java/xft/workbench/backstage/storehouseout/action/StorehouseoutAction.java
+++ jywz/src/main/java/xft/workbench/backstage/storehouseout/action/StorehouseoutAction.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import xft.workbench.backstage.base.action.ABSBaseController;
+import xft.workbench.backstage.base.enumeration.apply.PutinStatus;
+import xft.workbench.backstage.base.enumeration.apply.PutinType;
 import xft.workbench.backstage.base.enumeration.apply.PutoutStatus;
 import xft.workbench.backstage.base.enumeration.apply.PutoutType;
 import xft.workbench.backstage.base.util.EnumUtil;
@@ -44,7 +46,7 @@ public class StorehouseoutAction extends ABSBaseController{
      */
     @RequestMapping(value = "/storehouseout/downloadDetail.json")
     public @ResponseBody
-    String downloadDetail(HttpServletRequest request, HttpServletResponse response) {
+    String downloadDetailOut(HttpServletRequest request, HttpServletResponse response) {
         BufferedOutputStream bos = null;
         try {
             Map<String, Object> params = this.getRequestParams();
@@ -59,12 +61,12 @@ public class StorehouseoutAction extends ABSBaseController{
 			
             response.setCharacterEncoding("UTF-8");
             // 设置文档打开类型
-            response.setContentType("application/octet-stream;charset=UTF-8");
+            response.setContentType("application/octet-stream;charset=GBK");
             // 设置报文头为attachment响应类型
             response.setHeader("Content-disposition",
                     "attachment;" + FileUtil.encodeFileName(request, fileName));
 
-            byte[] bytes = this.createFile(sResult);  // 读取字节流
+            byte[] bytes = this.createFileOut(sResult);  // 读取字节流
 
             if (null == bytes || bytes.length == 0) {
                 throw new KPromptException("文件服务器上不存在此文件！");
@@ -96,7 +98,7 @@ public class StorehouseoutAction extends ABSBaseController{
     }
 	
      
-    private byte[] createFile(SqlResult sResult) throws Exception{
+    private byte[] createFileOut(SqlResult sResult) throws Exception{
     	StringBuffer fileContent = new StringBuffer("出库单号,出库类型,物资名称,物资类型,出库数量,规格型号,供应商,出库时间,出库仓库,出库状态");
     	fileContent.append("\r\n");
     	
@@ -114,11 +116,105 @@ public class StorehouseoutAction extends ABSBaseController{
     					.append("\r\n");
     	}
     	
-    	
-    	
-    	return fileContent.toString().getBytes("UTF-8");
+    	return fileContent.toString().getBytes();
     	
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /**
+     * 描述 : 文档下载、打开
+     * <p>
+     * 樊东新
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/storehousein/downloadDetail.json")
+    public @ResponseBody
+    String downloadDetailIn(HttpServletRequest request, HttpServletResponse response) {
+        BufferedOutputStream bos = null;
+        try {
+            Map<String, Object> params = this.getRequestParams();
+            bos = new BufferedOutputStream(response.getOutputStream());
+           
+            String fileName="入库明细-"+new SimpleDateFormat("yyyyMMdd").format(new Date())+".csv";
+            
+            GlobalMessage.addMapSessionInfo(params);
+			KResult result = comnService.comnQuery(params);
+			
+			SqlResult sResult=result.getSResult();
+			
+            response.setCharacterEncoding("UTF-8");
+            // 设置文档打开类型
+            response.setContentType("application/octet-stream;charset=GBK");
+            // 设置报文头为attachment响应类型
+            response.setHeader("Content-disposition",
+                    "attachment;" + FileUtil.encodeFileName(request, fileName));
+
+            byte[] bytes = this.createFileIn(sResult);  // 读取字节流
+
+            if (null == bytes || bytes.length == 0) {
+                throw new KPromptException("文件服务器上不存在此文件！");
+            }
+            response.setHeader("Content-Length", String.valueOf(bytes.length));
+
+            bos.write(bytes, 0, bytes.length);
+            return updateReturnJson(true, "文件下载成功", null);
+        } catch (Exception e) {
+            try {
+                response.setContentType("text/html;charset=UTF-8");
+                byte[] bb = this.updateErrorJson(e).getBytes();
+                bos.write(bb, 0, bb.length);
+            } catch (UnsupportedEncodingException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return null;
+        } finally {
+            try {
+                if (bos != null) {
+                    bos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+	
+     
+    private byte[] createFileIn(SqlResult sResult) throws Exception{
+    	StringBuffer fileContent = new StringBuffer("入库单号,入库类型,物资名称,物资类型,入库数量,规格型号,供应商,入库时间,入库仓库,入库状态");
+    	fileContent.append("\r\n");
+    	
+    	while(sResult.next()){
+    		fileContent.append(sResult.getString("putin_code")+",")
+    					.append(EnumUtil.getDescValueByEnumValue(PutinType.class,sResult.getString("putin_type"))+",")
+    					.append(sResult.getString("material_name")+",")
+    					.append(sResult.getString("material_type_name")+",")
+    					.append(sResult.getString("putin_number")+",")
+    					.append(sResult.getString("model")+",")
+    					.append(sResult.getString("supplier")+",")
+    					.append(sResult.getString("putin_date")+",")
+    					.append(sResult.getString("putin_storehouse_name")+",")
+    					.append(EnumUtil.getDescValueByEnumValue(PutinStatus.class,sResult.getString("putinstatus")))
+    					.append("\r\n");
+    	}
+    	
+    	return fileContent.toString().getBytes();
+    	
+    }
+    
+    
+    
 	
 	
 }
