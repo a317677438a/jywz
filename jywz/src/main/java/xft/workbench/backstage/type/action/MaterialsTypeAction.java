eab87de69913d139b769f2eafe1d93093f4f74dc
a317677438a <277235580@qq.com>
2017-10-15 21:56:44 +0800

    自动生成编码

--- jywz/src/main/java/xft/workbench/backstage/type/action/MaterialsTypeAction.java
+++ jywz/src/main/java/xft/workbench/backstage/type/action/MaterialsTypeAction.java
@@ -46,9 +46,7 @@ public class MaterialsTypeAction extends ABSBaseController{
 	public @ResponseBody String getMaterialType(){
 		try {
 			Integer num = typeBiz.getMaterialType();
-			if(num==0){
-				num = 1;
-			}
+			num++;
 			String numS = String.valueOf(num);
 			Integer size = numS.length();
 			if(numS.length()<4){
@@ -241,9 +239,7 @@ public class MaterialsTypeAction extends ABSBaseController{
 	public @ResponseBody String getMaterialNum(){
 		try {
 			Integer num = typeBiz.getMaterialNum();
-			if(num==0){
-				num = 1;
-			}
+			num++;
 			String numS = String.valueOf(num);
 			Integer size = numS.length();
 			if(numS.length()<6){
