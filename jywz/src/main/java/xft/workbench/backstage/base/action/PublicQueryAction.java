80bcb0bd1283b19e7cf2a2e54815d1a1f6dba13b
a317677438a <277235580@qq.com>
2017-10-15 23:31:07 +0800

    打版本代码

--- jywz/src/main/java/xft/workbench/backstage/base/action/PublicQueryAction.java
+++ jywz/src/main/java/xft/workbench/backstage/base/action/PublicQueryAction.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.apache.bcel.generic.NEW;
+
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
